265f07e8874524778fac4cb5ff093d064a7d320e
Kevin Earls <kevinearls@mac.com>
2023-11-30 16:50:43 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Kevin Earls on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
